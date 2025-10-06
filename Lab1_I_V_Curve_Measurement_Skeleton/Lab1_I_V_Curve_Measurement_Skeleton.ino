aa4b25b0cc0ed3d5b359f7bb1f8a120c99c441be
LinYunZhiYuan <cwangdw@connect.ust.hk>
2025-10-06 14:34:08 +0800

    Update Lab1_I_V_Curve_Measurement_Skeleton.ino

--- Lab1_I_V_Curve_Measurement_Skeleton/Lab1_I_V_Curve_Measurement_Skeleton.ino
+++ Lab1_I_V_Curve_Measurement_Skeleton/Lab1_I_V_Curve_Measurement_Skeleton.ino
@@ -1,8 +1,8 @@
 #include "Arduino.h"
 
-#define Vin 12 //*** Define the GPIO pin 12 as Vin ***
-#define Vout 27  //*** Define the GPIO pin 27 as Vout ***
-#define Cal 14//*** Define the GPIO pin 14 as the node for current calcultion ***
+#define Vin 2 //*** Define the GPIO pin 12 as Vin ***
+#define Vout 32  //*** Define the GPIO pin 27 as Vout ***
+//#define Cal 14//*** Define the GPIO pin 14 as the node for current calcultion ***
 
 //float R = ; //*** Resistance in Mohm ***
 
@@ -15,7 +15,7 @@ void setup() {
   Serial.begin(115200); //*** Define the data rate as 115200 ***
   pinMode(Vin, OUTPUT); //*** Set Vin as OUTPUT ***
   pinMode(Vout,INPUT); //*** Set Vout as INPUT ***
-  pinMode(Cal,INPUT); //*** Set Cal as INPUT ***
+  //pinMode(Cal,INPUT); //*** Set Cal as INPUT ***
   
   delay(100);   //*** Set 100 ms delay ***
   //This task only needs to be done once, thus it is placed in setup().
@@ -23,12 +23,13 @@ void setup() {
   for (int onPeriod = 0; onPeriod < 256; onPeriod++) 
     {
     
-    analogWrite(Vin,map(onPeriod,0,255,0,4095));// Output PWM at Vin      
+    //analogWrite(Vin,map(onPeriod,0,255,0,4095));// Output PWM at Vin  
+    analogWrite(Vin,onPeriod);// Output PWM at Vin      
     int Vout_value = analogRead(Vout); // read the voltage level at Vout 
-    int c_value = analogRead(Cal); // read the voltage level at the node for current calcultion  
+    //int c_value = analogRead(Cal); // read the voltage level at the node for current calcultion  
     //c=  ;//calculate the voltage at the node for current calcultion  
-    x = float(onPeriod)/255 ; // calculate duty cycle for TASK 1 or the voltage at Vout in mV for Task 2 3 4
-    y = Vout_value*3.3/4095; // calculate the voltage at Vout in V for TASK 1 or current in uA for Task 2 3 4
+    x = float(onPeriod)/255.0f ; // calculate duty cycle for TASK 1 or the voltage at Vout in mV for Task 2 3 4
+    y = Vout_value*3.3f/4095.0f; // calculate the voltage at Vout in V for TASK 1 or current in uA for Task 2 3 4
     Serial.print(x); 
     Serial.print(","); // seperate values by comma 
     Serial.println(y);
